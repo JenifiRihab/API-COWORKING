2bbfe2af225167abcc8a5f611973ba95dbba2dba
jenifi_rihab <rihab.jenifi@gmail.fr>
2023-07-13 12:43:06 +0200

    first commit

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello Affichage de tous les coworkings!')
+app.get('/api/coworking/.id', (req, res) => {
+  console.log(req.params.id)
+  res.send(`Affichage les coworkings numero: ${req}`)
 })
 
 app.listen(port, () => {
