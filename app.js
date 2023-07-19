1d65d20c0450026d6d11f49dbca1514612cdfc1e
jenifi_rihab <rihab.jenifi@gmail.fr>
2023-07-19 10:34:57 +0200

    my commit

--- app.js
+++ app.js
@@ -4,12 +4,69 @@ const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+// const{ Sequelize, DataTypes } = require ('sequelize');
+
+// const sequelize = new Sequelize('coworking-2023', 'root', '',{
+// host: 'localhost',
+// dialect:'mariadb',
+// logging: false
+// });
+
+// sequelize.authenticate()
+// .then(() => console.log('la connexion a la base de données a bien été établie.'))
+// .catch(error => console.log('impossible de se connecter a la base de données ${error}'))
+
+// const User = sequelize.define('User', {
+//   username: DataTypes.STRING,
+//   firstname: DataTypes.STRING,
+//   birthday: DataTypes.DATE,
+//   address: DataTypes.STRING,
+//   job: DataTypes.STRING,
+//   email: DataTypes.STRING,
+// });
+
+// sequelize
+// .sync({force: true})
+// .then(() =>{
+//     User.create({
+//         username: 'rihab',
+//         firstname: 'jenifi',
+//         birthday: new Date('mars 04, 1998, 01:00:00'),
+//         address:'16 rue du Docteur Albert Schweitzer 33300 bordeaux',
+//         job:'Dev-web',
+//         email: 'rihab.jenifi@icloud.com',
+       
+
+//     })
+// })
+
+// const coworkingModels = require('./models/coworkingModel')
+// const coworking = coworkingModels(sequelize, DataTypes)
+
+// sequelize
+// .sync({force: true})
+// .then(() =>{
+//     mockCoworkings.forEach(mock =>{
+//     coworking.create({
+//         id: 12,
+//         name:'Oasis Coworking',
+//         price: { "hour": 4, "day": 21, "month": 100 },
+//         superficy: 200,
+//         capacity: 27,
+//         address: { "number": "68bis", "street": "avenue Jean Jaurès", "postCode": 33150, "city": "Cenon" },
+    
+//     })
+//    })
+// })
+
+
+
+
 app.use(morgan('dev'))
 app.use(express.json())
 
 const coworkingRouter = require('./routes/coworkingRoutes')
 app.use('/api/coworkings', coworkingRouter)
-
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
